2b64c31287bfe8f66c96285f47f3a1c180c1dfc0
abhinavmaharana <abhinavmaharana.004@gmail.com>
2023-09-05 13:18:23 +0530

    ✅ TMDB api setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import './App.css'
+/* eslint-disable no-unused-vars */
+import { useState, useEffect } from "react"
+
+import {fetchDataFromApi} from './utils/api';
 
 function App() {
+
+  useEffect(() => {
+    apiTesting()
+  }, [])
+  
+  const apiTesting = () => {
+    fetchDataFromApi('/movie/popular')
+      .then((res) => {
+        console.log(res)
+      })
+  }
   return (
-    <>
-      
-    </>
+    <div>
+      <h1>Api Testing</h1>
+
+    </div>
   )
 }
 
